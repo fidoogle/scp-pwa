402e5dcdc8d6e4d301c1785ea9d7eba4449a076b
Fidel Guajardo <figuajardo@saws.org>
2019-12-23 14:50:38 -0600

    add generic login button

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import Nav from '../components/nav'
+import ButtonLink from '../components/mui-next-button';
 
 const styles = theme => ({
   button: {
@@ -17,34 +17,13 @@ function IndexPage(props) {
   const { classes } = props;
   return (
     <>
-      <Nav/>
-      <Button variant="contained" className={classes.button}>
-        Default
-      </Button>
-      <Button variant="contained" color="primary" className={classes.button}>
-        Primary
-      </Button>
-      <Button variant="contained" color="secondary" className={classes.button}>
-        Secondary
-      </Button>
-      <Button variant="contained" color="secondary" disabled className={classes.button}>
-        Disabled
-      </Button>
-      <Button variant="contained" href="#contained-buttons" className={classes.button}>
-        Link
-      </Button>
-      <input
-        accept="image/*"
-        className={classes.input}
-        id="contained-button-file"
-        multiple
-        type="file"
-      />
-      <label htmlFor="contained-button-file">
-        <Button variant="contained" component="span" className={classes.button}>
-          Upload
-        </Button>
-      </label>
+      <Button 
+        component={ButtonLink} 
+        href={'/my-bill'}
+        variant="contained" 
+        color="primary" 
+        className={classes.button}
+      >Login</Button>
     </>
   );
 }
